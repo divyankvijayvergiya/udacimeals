4a8f042a4d854a7332314490f36a1d997f618b67
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2017-12-16 01:49:44 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { addRecipe, removeFromCalendar } from '../actions';
+import { connect } from 'react-redux';
+import { capitalize } from '../Utils/helper';
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
+
+class App extends Component {
+  doThing = () =>{
+    this.props.selectRecipe({})
+  }
+  render(){
+    const { calendar, remove } = this.props
+    const mealOrder = ['breakfast', 'lunch', 'dinner']
+
+    return(
+      <div className= 'container'>
+         <ul className= 'meal-type'>
+           {mealOrder.map((mealType) => (
+             <li key={ mealType } className= 'subheader'>
+                 {capitalize(mealType)}
+
+             </li>
+           ))}
+         </ul>
+     </div>
+    )
+  }
+}
+
+
+function mapStateToProps ({ calendar, food }) {
+   const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+   return {
+     calendar: dayOrder.map((day) => ({
+       day,
+       meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+         meals[meal] = calendar[day][meal]
+           ? food[calendar[day][meal]]
+           : null
+
+         return meals
+       }, {})
+     })),
+   }
+ }
+
+function mapDispatchToProps(dispatch){
+  return{
+    selectRecipe: (data) =>dispatch(addRecipe(data)),
+    remove: (data)=> dispatch(removeFromCalendar(data)),
+
+    }
+}
+
+ export default connect(
+   mapStateToProps, mapDispatchToProps
+ )(App)
